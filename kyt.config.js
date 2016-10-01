be20bc71eb9992a24cb947011869432b44d2e5e2
delambo <delambo@gmail.com>
2016-10-01 14:04:29 -0400

    first pass at static html file

--- kyt.config.js
+++ kyt.config.js
@@ -1,8 +1,16 @@
 
-// Base kyt config.
-// Edit these properties to make changes.
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   reactHotLoader: true,
   debug: false,
+  noServer: true,
+  modifyWebpackConfig: (config, options) => {
+    if (options.type === 'client') {
+      config.plugins.push(new HtmlWebpackPlugin({
+        template: 'src/index.ejs'
+      }));
+    }
+    return config;
+  }
 };
